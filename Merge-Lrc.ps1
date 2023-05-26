b24dbf625eb057488b0d72f57f88f21f9caaf352
Aoba Xu <xuyaqi888@live.cn>
2023-05-26 15:33:45 +0800

    first commit

---
+++ Merge-Lrc.ps1
@@ -0,0 +1,156 @@
+<#
+.SYNOPSIS
+Merge lrc files.
+.DESCRIPTION
+Merge lrc files.
+.PARAMETER Path
+Path to one or more locations. Wildcards are permitted.
+.PARAMETER MergeMethod
+Merge method. Default is 'Merge'. 'Merge' is to merge all lines with the same time. 'Intersect' is to merge all lines with the same time and split the lines with different time evenly. 'Union' is to merge all lines with the same time and split the lines with different time.
+.PARAMETER SplitChar
+Split character. Default is ' '.
+.INPUTS
+System.String[]
+.OUTPUTS
+Lrc file.
+.EXAMPLE
+PS C:\> Merge-Lrc -Path "C:\Users\user\Music\*.lrc" -MergeMethod "Merge" -SplitChar " "
+This example merges all lrc files in the folder "C:\Users\user\Music\".
+.EXAMPLE
+PS C:\> Merge-Lrc -Path "C:\Users\user\Music\*.lrc" -MergeMethod "Intersect" -SplitChar " " | Out-File "C:\Users\user\Music\merged.lrc"
+This example merges all lrc files in the folder "C:\Users\user\Music\" and saves the result to "C:\Users\user\Music\merged.lrc".
+#>
+
+#Requires -Version 7
+using namespace System.Collections.Generic
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true,
+        HelpMessage = "Path to one or more locations. Wildcards are permitted.")]
+    [ValidateNotNullOrEmpty()]
+    [SupportsWildcards()]
+    [Alias("PSPath")]
+    [string[]]
+    $Path,
+    [Parameter(
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true,
+        HelpMessage = "Merge method. Default is 'Merge'.")]
+    [ValidateSet("Merge", "Intersect", "Union")]
+    $MergeMethod = "Merge",
+    [Parameter(
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true,
+        HelpMessage = "Split character. Default is ' '.")]
+    $SplitChar = " "
+)
+
+class Lrc {
+    [SortedDictionary[int, List[string]]] $Lines
+}
+
+function Get-Lrc ($Path) {
+    $files = Get-ChildItem -Path $Path
+    return $files | ForEach-Object {
+        $lrc = [Lrc]::new()
+        $lrc.Lines = [SortedDictionary[int, List[string]]]::new()
+        $lines = Get-Content -Path $_.FullName
+        $lines | ForEach-Object {
+            $line = $_
+            $lineContent = [regex]::Match($line, '\[(\d+:\d+\.\d+)\](.*)')
+            if ($lineContent.Success) {
+                $time = [timespan]::ParseExact($lineContent.Groups[1].Value, 'mm\:ss\.ff', [cultureinfo]::GetCultureInfo('en-US')).TotalMilliseconds
+                if ($lrc.Lines.ContainsKey($time)) {
+                    $lrc.Lines[$time].Add($lineContent.Groups[2].Value)
+                }
+                else {
+                    $list = [List[string]]::new()
+                    $list.Add($lineContent.Groups[2].Value)
+                    $lrc.Lines.Add($time, $list)
+                }
+            }
+        }
+        return $lrc
+    }
+}
+
+function Merge-Lrc ($Lrcs) {
+    if ($Lrcs.Count -eq 0) {
+        return [Lrc]::new()
+    }
+    if ($Lrcs.Count -eq 1) {
+        return $Lrcs[0]
+    }
+    $mergedLrc = $Lrcs[0]
+    $Lrcs = $Lrcs | Select-Object -Skip 1
+    $Lrcs | ForEach-Object {
+        $lrc = $_
+        $lrc.Lines.Keys | ForEach-Object {
+            $_
+            $time = $_
+            if ($mergedLrc.Lines.ContainsKey($time)) {
+                $mergedLrc.Lines[$time].AddRange($lrc.Lines[$time])
+            }
+            else {
+                $mergedLrc.Lines.Add($time, $lrc.Lines[$time])
+            }
+        }
+    }
+    return $mergedLrc
+}
+
+function Save-Lrc ($Lrc, $MergeMethod = "Merge", $SplitChar = " ") {
+    switch ($MergeMethod) {
+        "Merge" {
+            return $Lrc.Lines.Keys | ForEach-Object {
+                $time = $_
+                "[$([timespan]::FromMilliseconds($time).ToString('mm\:ss\.ff'))]$([string]::Join($SplitChar, $Lrc.Lines[$time]))"
+            }
+        }
+        "Intersect" {
+            $times = $Lrc.Lines.Keys | Select-Object
+            $out = [List[string]]::new()
+            for ($i = 0; $i -lt $times.Count; $i++) {
+                $lines = $Lrc.Lines[$times[$i]] | Select-Object
+                if ($lines.Count -eq 1) {
+                    $out.Add("[$([timespan]::FromMilliseconds($times[$i]).ToString('mm\:ss\.ff'))]$($lines[0]))")
+                }
+                else {
+                    if ($i -eq ($times.Count - 1)) {
+                        $currentTime = $times[$i]
+                        $interval = 100
+                        for ($j = 0; $j -lt $lines.Count; $j++) {
+                            $out.Add("[$([timespan]::FromMilliseconds($currentTime + $j * $interval).ToString('mm\:ss\.ff'))]$($lines[$j])")
+                        }
+                    }
+                    else {
+                        $currentTime = $times[$i]
+                        $nextTime = $times[$i + 1]
+                        $interval = ($nextTime - $currentTime) / ($lines.Length)
+                        for ($j = 0; $j -lt $lines.Count; $j++) {
+                            $out.Add("[$([timespan]::FromMilliseconds($currentTime + $j * $interval).ToString('mm\:ss\.ff'))]$($lines[$j])")
+                        }
+                    }
+                }
+            }
+            return $out
+        }
+        "Union" {
+            return $Lrc.Lines.Keys | ForEach-Object {
+                $time = $_
+                $Lrc.Lines[$time] | ForEach-Object {
+                    "[$([timespan]::FromMilliseconds($time).ToString('mm\:ss\.ff'))]$_"
+                }
+            }
+        }
+    }
+}
+$lrcs = Get-Lrc -Path $Path
+if ($lrcs.Count -eq 0) {
+    Write-Error "No lrc file found."
+    return
+}
+$mergedLrc = Merge-Lrc -Lrcs $lrcs
+return Save-Lrc -Lrc $mergedLrc -MergeMethod $MergeMethod -SplitChar $SplitChar
---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Merge-Lrc
+
+## 介绍
+
+这是一个用于合并歌词的小工具，可以将多个歌词文件合并成一个歌词文件，也可以将多个歌词文件合并到一个音频文件中。
+
+## TL;DR
+
+```powershell
+.\Merge-Lrc.ps1 -Path "C:\Users\user\Music\*.lrc" -MergeMethod "Merge" -SplitChar " " | Out-File "C:\Users\user\Music\merged.lrc"
+```
+
+## 参数说明
+
+| 参数名      | 是否可选 | 默认值 | 说明                                                                                                                                             |
+| ----------- | -------- | ------ | ------------------------------------------------------------------------------------------------------------------------------------------------ |
+| Path        | 否       | 无     | 歌词文件路径，支持通配符。                                                                                                                       |
+| MergeMethod | 是       | Merge  | 合并方法。Merge 为合并所有时间相同的行，Intersect 为合并所有时间相同的行并将时间不同的行均分，Union 为合并所有时间相同的行并将时间不同的行合并。 |
+| SplitChar   | 是       | 空格   | 分隔符。                                                                                                                                         |
+
+## 示例
+
+- 将文件夹中的所有歌词文件合并成一个歌词文件
+
+```powershell
+.\Merge-Lrc.ps1 -Path "C:\Users\user\Music\*.lrc" -MergeMethod "Merge" -SplitChar " "
+```
+
+- 将文件夹中的所有歌词文件合并成一个歌词文件并保存
+
+```powershell
+.\Merge-Lrc.ps1 -Path "C:\Users\user\Music\*.lrc" -MergeMethod "Merge" -SplitChar " " | Out-File "C:\Users\user\Music\merged.lrc"
+```
+
+- 将一个混乱的歌词文件整理合并并保存
+
+```powershell
+.\Merge-Lrc.ps1 -Path "C:\Users\user\Music\test.lrc" -MergeMethod "Intersect" -SplitChar " " | Out-File "C:\Users\user\Music\test.lrc"
+```
